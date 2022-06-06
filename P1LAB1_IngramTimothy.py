9eff2f499f829fe305f376d8447e871dc7b1f8d9
Timothy Ingram <106992112+8BitTech2@users.noreply.github.com>
2022-06-06 17:46:08 -0400

    Add files via upload
    Phython assignment for CTI110 (summer2022) P1LAB1 - Learning to fix code errors.

---
+++ P1LAB1_IngramTimothy.py
@@ -0,0 +1,4 @@
+userNum = int(input())
+userNumSquared = userNum * userNum   # Bug here; fix it when instructed
+   
+print(userNumSquared)       # Output formatting issue here; fix it when instructed
