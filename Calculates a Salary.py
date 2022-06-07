e27dd6dfd0817c1ba26fb94195c3b67b6ae3f500
Timothy Ingram <106992112+8BitTech2@users.noreply.github.com>
2022-06-06 21:32:25 -0400

    Add files via upload
    A simple Python program that will calculate your salary based on an hourly wage!

---
+++ Calculates a Salary.py
@@ -0,0 +1,11 @@
+# Calculates a Salary based on a given hourly wage
+wage = 20
+hours = 40
+weeks = 52
+salary = wage * hours * weeks
+
+print ('Salary is:', salary)
+
+hours = 35
+salary = wage * hours * weeks
+print ('New salary is:', salary)
